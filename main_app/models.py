749c07a0de6ea0bc3bf904e1972019baa0ff71e0
BRAKEL3SS <rck496@yahoo.com>
2022-04-13 14:21:24 -0700

    adds Socket model

--- main_app/models.py
+++ main_app/models.py
@@ -2,10 +2,22 @@ from django.db import models
 from django.urls import reverse
 
 # Create your models here.
+class Socket(models.Model):
+    size = models.IntegerField()
+    drive = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.size
+    
+    def get_absolute_url(self):
+        return reverse('sockets_detail', kwargs={'pk': self.id})
+
+
 class Bike(models.Model):
     brand = models.CharField(max_length=100)
     model = models.CharField(max_length=100)
     hours = models.DecimalField(max_digits=6, decimal_places=2)
+    tools = models.ManyToManyField(Socket)
 
     def __str__(self):
         return self.model
